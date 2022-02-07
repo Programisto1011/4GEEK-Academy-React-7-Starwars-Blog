96b1e51295d2d658d55857ae65e5ceadad4f2f0f
jseiglie <javierseiglie@gmail.com>
2022-02-07 16:07:29 +0000

    unknown changes

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -4,47 +4,47 @@ import { useContext } from "react";
 import { Context } from "../store/appContext.js";
 import Card from "../component/Card.jsx";
 
-
 export const Home = () => {
-	const {store, actions} = useContext(Context)
+  const { store, actions } = useContext(Context);
 
-	useEffect(() => {
-		actions.LoadDataPlanet()
-	}, []);
+  useEffect(() => {
+    actions.LoadDataPlanet();
+  }, []);
 
-	useEffect(() => {
-		actions.LoadDataPeople()
-	}, []);
+  useEffect(() => {
+    actions.LoadDataPeople();
+  }, []);
 
-	return (
-	<>
-	
-	<div className="row">
-            {store.peopleData.map((peopleData, index)=> {
-                return(	
-					<>
-                    <div className="d-flex col-3">
-						<Card key={index} title={peopleData.name} gender={peopleData.gender} height={peopleData.height}/>
-						</div>    
-					
-					</>
-                )
-            })}
-			</div>
+  return (
+    <>
+      <div className="row">
+        {store.peopleData.map((peopleData, index) => {
+          return (
+            <>
+              <div className="d-flex col-3">
+                <Card
+                  key={index}
+                  title={peopleData.name}
+                  gender={peopleData.gender}
+                  height={peopleData.height}
+                />
+              </div>
+            </>
+          );
+        })}
+      </div>
 
-			<div className="row">
-            {store.planetData.map((planetData, index)=> {
-                return(	
-					<>
-                    <div className="d-flex col-3">
-						<Card key={index} title={planetData.name}/>
-						</div>    
-					
-					</>
-                )
-            })}
-			</div>
-
-	</>
-	
-)};
+      <div className="row">
+        {store.planetData.map((planetData, index) => {
+          return (
+            <>
+              <div className="d-flex col-3">
+                <Card key={index} title={planetData.name} />
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
