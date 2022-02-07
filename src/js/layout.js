f7cb1b2073d0880108a4235333ef196570c6aa12
jseiglie <javierseiglie@gmail.com>
2022-02-07 10:57:09 +0000

    Data fetched

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
+
+import { Home } from "./views/Home.jsx";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Navbar } from "./component/Navbar.jsx";
+
 
 //create your first component
 const Layout = () => {
@@ -19,7 +19,7 @@ const Layout = () => {
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
+				
 					<Navbar />
 					<Switch>
 						<Route exact path="/">
@@ -35,8 +35,8 @@ const Layout = () => {
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
-					<Footer />
-				</ScrollToTop>
+					
+				
 			</BrowserRouter>
 		</div>
 	);
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,21 +1,62 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			urlAPI: 'https://www.swapi.tech/api',
+			peopleData: [],
+			planetData: []
 		},
+
 		actions: {
 			// Use getActions to call a function within a fuction
+
+			// Cargar Personajes
+			LoadDataPeople: () => {
+				//console.log((getStore().urlAPI).concat('/people'))
+			fetch((getStore().urlAPI).concat('/people'), {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Loading People Failed')
+			})
+			.then((res) => {
+				setStore({peopleData:res.results})
+					
+			})
+			.catch((err) => console.error(err)); 
+			},
+
+// Cargar Planetas 
+
+LoadDataPlanet: () => {
+	//console.log((getStore().urlAPI).concat('/planets'))
+fetch((getStore().urlAPI).concat('/planets'), {
+method: "GET",
+headers: {
+	Accept: "application/json",
+},
+})
+.then((response) => {
+	//console.log(response);
+	if (response.ok) {
+		return response.json();
+	}
+	throw new Error('Loading Planets Failed')
+})
+.then((res) => {
+	setStore({planetData:res.results});
+	console.log({planetData:res.results})
+		
+})
+.catch((err) => console.error(err)); 
+},
+
+
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
